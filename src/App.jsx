e3b3da6714a1782902889ff010f0c4584b06954e
farazazeem-6 <faraz.tacticminds.com>
2025-10-10 17:31:07 +0500

    add-toggle-btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header'
-import Hero from './components/Hero'
+import React, { createContext, useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
 
 function App() {
+  const [theme, setTheme] = useState("light");
+  function toggleTheme() {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  }
   return (
     <div>
-      <Header/>
-      <Hero/>
+        <Header />
+        <Hero />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
